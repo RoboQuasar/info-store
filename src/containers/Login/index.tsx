3b4d3a0366e8f0392a1dba3e1a5d7854d0b6eb80
Roman Sineokov <roboquasar@gmail.com>
2019-09-02 16:29:55 +0300

    Added react-bootstrap form in LoginPopup

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,5 +1,10 @@
 import React, { PureComponent } from 'react';
 
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+import styles from './index.module.css';
+
 class Login extends PureComponent {
   handleSubmit() {
     console.log('kuku');
@@ -7,11 +12,23 @@ class Login extends PureComponent {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="имя пользователя" />
-        <input type="text" placeholder="пароль" />
-        <button type="submit" aria-label="Войти">Войти</button>
-      </form>
+      <Form className={styles.form}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Логин</Form.Label>
+
+          <Form.Control type="email" placeholder="Введите e-mail" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Пароль</Form.Label>
+
+          <Form.Control type="password" placeholder="введите пароль" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Войти
+        </Button>
+      </Form>
     );
   }
 }
